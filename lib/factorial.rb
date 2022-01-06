35e5b474a26421e32db43a2c470910875ecedb62
Santanu Bhattacharya <santanu.bhattacharya@quantuminventions.com>
2022-01-06 09:30:16 +0530

    fix indentation

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Factorial
   def self.result(num)
-  	time = Time.now
-  	fact = 1
+    time = Time.now
+    fact = 1
 
-  	if(num==0)
-  	  fact = 1
-  	else
-  	  i = 1
+    if num.zero?
+      fact = 1
+    else
+      i = 1
 
-  	  while(i <=num)
-  	  	fact = fact * i
-  	  	i = i + 1
-  	  end
-  	end
+      while i <= num
+        fact *= i
+        i += 1
+      end
+    end
 
-  	puts "time taken: #{Time.now - time} seconds"
-  	puts "Factorial of #{num} is #{fact}"
+    puts "time taken: #{Time.now - time} seconds"
+    puts "Factorial of #{num} is #{fact}"
+    fact
   end
 end
